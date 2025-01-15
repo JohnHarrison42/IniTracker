65b8ffdccf294a33b8ec56cd3ecfdeec25da6875
JohnHarrison42 <PrimeDirective1701@protonmail.com>
2025-01-15 20:49:48 +0000

    :wq

--- IniTracker_Better.py
+++ IniTracker_Better.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 # Initial data for the pool of characters
 initial_pool = [
-    {"ID": 1, "Name": "Aragorn", "Armor Class": 16},
+    {"ID": 1, "Name": "Aragornnnnnnnnn", "Armor Class": 16},
     {"ID": 2, "Name": "Legolas", "Armor Class": 15},
     {"ID": 3, "Name": "Gimli", "Armor Class": 17},
     {"ID": 4, "Name": "Frodo", "Armor Class": 12},
